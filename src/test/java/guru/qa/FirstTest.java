58792ebe2cfb3a4d59c51f31b6e222c4ccb316c0
Tatsiana Vaitsekhouskaya <t.vaitsekhouskaya@rozumsoft.by>
2022-08-01 21:57:09 +0300

    first commit

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,17 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FirstTest {
+
+    @Test
+    void clickTest() {
+        open("https://www.google.com/");
+        $(By.name("q")).sendKeys("Hello");
+
+    }
+}
